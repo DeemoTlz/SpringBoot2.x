f3c5e55a89ca73b99b8fabb221a195756cce3f9c
Deemo <825532516@qq.com>
2018-11-26 16:49:32 +0800

    调整项目依赖问题，保证项目正常运行

--- albrus-main/src/main/java/com/albrus/main/AlbrusMainApplication.java
+++ albrus-main/src/main/java/com/albrus/main/AlbrusMainApplication.java
@@ -2,6 +2,7 @@ package com.albrus.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.albrus.*"})
 public class AlbrusMainApplication {
