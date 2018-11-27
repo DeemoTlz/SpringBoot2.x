f23da5e787b5ff0ec28088773bde6cb4bf765ea3
Deemo <825532516@qq.com>
2018-11-27 18:05:26 +0800

    打包前台至本项目，保证正常运行

--- albrus-shiro/src/main/java/com/albrus/shiro/controller/LoginController.java
+++ albrus-shiro/src/main/java/com/albrus/shiro/controller/LoginController.java
@@ -1,16 +1,30 @@
 package com.albrus.shiro.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.albrus.shiro.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class LoginController {
 
-    @RequestMapping("/login")
+    @GetMapping(value= {"/", "/index"})
+    public String index() {
+        System.out.println("welcome!");
+
+        return "index";
+    }
+
+    @GetMapping("/login")
     public String login(@RequestParam("user") String user, @RequestParam("password") String password) {
         System.out.println("user:" + user + ", password:" + password);
 
-        return null;
+        return "{\"code_\": 0}";
+    }
+
+    @PostMapping(value = "/login2")
+    public String login(@RequestBody User user) {
+        System.out.println("user: " + user);
+
+        return "{\"code_\": 0}";
     }
 }
