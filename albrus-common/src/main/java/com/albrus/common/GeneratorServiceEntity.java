48d55d90eb09fb489575cc430e353a6cf66cc920
Deemo <825532516@qq.com>
2018-12-10 15:00:16 +0800

    优化代码生成器

--- albrus-common/src/main/java/com/albrus/common/GeneratorServiceEntity.java
+++ albrus-common/src/main/java/com/albrus/common/GeneratorServiceEntity.java
@@ -11,13 +11,14 @@ import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 public class GeneratorServiceEntity {
 
-    public static void main(String args[]) {
-        String packageName = "com.albrus.consume";
-        // generateByTables(false, packageName, "albrus_consume_cost");
-        generateByTables(packageName, "albrus_consume_cost");
+    public static void main(String[] args) {
+        String packageName = "com.albrus";
+        String modelName = "shiro";
+
+        generateByTables(packageName, modelName, "albrus_user");
     }
 
-    private static void generateByTables(boolean serviceNameStartWithI, String packageName, String... tableNames) {
+    private static void generateByTables(String packageName, String modelName, String... tableNames) {
         GlobalConfig config = new GlobalConfig();
         String dbUrl = "jdbc:mysql://127.0.0.1:3306/albrus_account?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&serverTimezone=GMT%2B8";
         DataSourceConfig dataSourceConfig = new DataSourceConfig();
@@ -49,11 +50,11 @@ public class GeneratorServiceEntity {
                 .setBaseColumnList(true)
                 .setAuthor("albrus")
                 .setEnableCache(false)
-                .setOutputDir("D:\\WorkSpace\\IDEA\\albrusAccount\\albrus-consume\\src\\main\\java")
+                .setOutputDir("D:\\WorkSpace\\IDEA\\albrusAccount\\albrus-" + modelName + "\\src\\main\\java")
                 .setFileOverride(true);
-        if (!serviceNameStartWithI) {
+        /*if (!serviceNameStartWithI) {
             config.setServiceName("%sService");
-        }
+        }*/
         new AutoGenerator().setGlobalConfig(config)
                 .setTemplateEngine(new FreemarkerTemplateEngine())
                 .setDataSource(dataSourceConfig)
@@ -61,13 +62,10 @@ public class GeneratorServiceEntity {
                 .setPackageInfo(
                         new PackageConfig()
                                 .setParent(packageName)
+                                .setModuleName(modelName)
                                 .setController("controller")
                                 .setEntity("entity")
                                 .setXml("mapper.mapping")
                 ).execute();
-    }
-
-    private static void generateByTables(String packageName, String... tableNames) {
-        generateByTables(true, packageName, tableNames);
     }
 }
